1479f41d3f191911a99b491be4116d75bbd51dbf
Ajibose <ajiboseibrahim12@gmail.com>
2023-11-30 14:21:55 +0000

    Task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Request to a URL and display the size of the body of the response
-url=$(nslookup $1)
-curl -w "%{size_download}" $locator
+url=$(nslookup "$1")
+curl -w "%{size_download}" "$url"
