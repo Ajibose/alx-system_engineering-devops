ecea48e46ff53d76cd23dd593f8a0fd3fde17e7f
Ajibose <ajiboseibrahim12@gmail.com>
2024-01-03 19:52:52 +0000

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ def retrieve_todo(emp_id):
     """Retrive the todo list for a user"""
     r = requests.get(
             f"https://jsonplaceholder.typicode.com/users/{emp_id}")
-    emp_name = r.json()["name"]
+    emp_name = r.json().get("name")
 
     params = {"userId": emp_id}
     r = requests.get(
@@ -21,7 +21,7 @@ def retrieve_todo(emp_id):
     completedList = []
     for todo in todo_list:
         if todo['completed'] is True:
-            completedList.append(todo["title"])
+            completedList.append(todo.get("title"))
     completedTask = len(completedList)
 
     print("Employee {} is done with tasks({}/{}):\n\t"
