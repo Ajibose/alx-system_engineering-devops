0df124ec7b9f2e8c5778c2418656406ad3fa2bf3
Ajibose <ajiboseibrahim12@gmail.com>
2024-01-03 19:44:12 +0000

    Task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,28 +9,26 @@ import requests
 def retrieve_todo(emp_id):
     """Retrive the todo list for a user"""
     r = requests.get(
-            f"https://jsonplaceholder.typicode.com/users/",
-            params={'id': emp_id})
-    content = r.json()
-    emp_name = content[0]['name']
+            f"https://jsonplaceholder.typicode.com/users/{emp_id}")
+    emp_name = r.json()["name"]
 
-    r = requests.get(
-            f"https://jsonplaceholder.typicode.com/todos/",
-            params={'userId': emp_id})
-    total_size = len(r.json())
-    params = {'userId': emp_id, 'completed': 'true'}
-
+    params = {"userId": emp_id}
     r = requests.get(
             f"https://jsonplaceholder.typicode.com/todos/", params=params)
+
     todo_list = r.json()
-    completed_size = len(todo_list)
+    totalTasks = len(todo_list)
+    completedList = []
+    for todo in todo_list:
+        if todo['completed'] is True:
+            completedList.append(todo["title"])
+    completedTask = len(completedList)
+
     print(
             f"Employee {emp_name} is done with "
-            f"tasks({completed_size}/{total_size}):"
+            f"tasks({completedTask}/{totalTasks}):"
             )
-
-    for i in todo_list:
-        print(f"\t {i['title']}")
+    for todo in completedList:  print(f"\t {todo}")
 
 
 if __name__ == "__main__":
