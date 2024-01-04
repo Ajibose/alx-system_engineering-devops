f76f88f286f6faf4fd4a2fb032d02cd903f67ecd
Ajibose <ajiboseibrahim12@gmail.com>
2024-01-04 02:37:54 +0000

    Task3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+    Retrieve data from API
+"""
+import json
+import requests
+import sys
+
+
+def retrieve_todo():
+    """Retrive the todo list for a user"""
+    r = requests.get(
+            f"https://jsonplaceholder.typicode.com/users")
+    usersDict = {}
+    content = r.json()
+    for user in content:
+        usersDict[f"{user.get('id')}"] = user.get('username')
+    # print(usersDict)
+
+    r = requests.get(
+            f"https://jsonplaceholder.typicode.com/todos/")
+
+    allTasks = {}
+    for userId, user in usersDict:
+        allTasks[f"{userId}"] = []
+
+    with open(f"todo_all_employees.json", "w", newline='') as f:
+        todos = r.json()
+        for todo in todos:
+            userId = todo.get('userId')
+            allTasks[f"{userId}"].append({"username": f"{usersDict[f'{userId}']}", "task": f"{todo.get('title')}", "completed": todo.get('completed')})
+        json.dump(allTasks, f)
+
+if __name__ == "__main__":
+    """if len(sys.argv) < 2:
+        sys.exit()"""
+
+    # emp_id = sys.argv[1]
+
+    """if type(eval(emp_id)) != int:
+        sys.exit()"""
+
+    retrieve_todo()
