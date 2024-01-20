14c3b32943a2508e481b6face632b07c11ca7c5a
Ajibose <ajiboseibrahim12@gmail.com>
2024-01-20 01:47:41 +0000

    Task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Get total subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, headers={'User-agent': 'Alx'},
-            allow_redirects=False)
+    response = requests.get(url, headers={'user-agent': 'Alx'})
     if response.status_code == 200:
         return response.json()["data"]["subscribers"]
     else:
